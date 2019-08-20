bc52636f4cd0d1f11aaf0f127b05cf767d841f01
Aditya Giri <dtrg21@gmail.com>
2019-08-20 10:11:08 +0530

    Add Todos

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <section className="todoapp">
+			<header className="header">
+				<h1>todos</h1>
+				<input className="new-todo" placeholder="What needs to be done?" autofocus />
+			</header>
+			<section className="main">
+				<input id="toggle-all" className="toggle-all" type="checkbox" />
+				<label htmlFor="toggle-all">Mark all as complete</label>
+  
+
+        {
+          /**
+           * This part needs to be edited.
+           */
+        }
+				<ul className="todo-list"></ul>
+
+
+
+			</section>
+			<footer className="footer">
+				<span className="todo-count"></span>
+				<ul className="filters">
+					<li>
+						<a href="#/" className="selected">All</a>
+					</li>
+					<li>
+						<a href="#/active">Active</a>
+					</li>
+					<li>
+						<a href="#/completed">Completed</a>
+					</li>
+				</ul>
+				<button className="clear-completed">Clear completed</button>
+			</footer>
+		</section>
   );
 }
 
